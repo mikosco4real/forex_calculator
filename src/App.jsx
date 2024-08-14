a5509cea1bd3c4caf055a7d856daa2c08d3e2dfd
Michael Okolo <okolomikel2009@gmail.com>
2024-08-14 17:10:46 +1000

    Initialized repository

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalculatorForm from './components/CalculatorForm'
+
+function App() {
+
+  return (
+    <>
+      <div> <CalculatorForm /> </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CalculatorForm.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export default function CalculatorForm() {
+  const [formData, setFormData] = useState({})
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+    </div>
+  );
+}
